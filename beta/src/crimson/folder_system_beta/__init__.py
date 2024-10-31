7a43d1a5d69218be1a258ff7241fba3ae393861f
crimson206 <sisung.kim1@gmail.com>
2024-10-31 20:08:23 +0100

    implementation

--- beta/src/crimson/folder_system_beta/__init__.py
+++ beta/src/crimson/folder_system_beta/__init__.py
@@ -1,1 +1,14 @@
-# Init file for the module
+"""
+---
+```
+Filter:
+    - summary: Filter path to search
+    - description: Use `FnFilter` or `ReFilter` as predefined `Filter`,
+    or define your own `Filter`.
+search:
+    - summary: search folder or path
+```
+---
+"""
+from crimson.folder_system_beta.filter import Filter, FnFilter, ReFilter
+from crimson.folder_system_beta.search import search
